26d964ed2c7adae9d26c2d907bf8ab17c47dbaaa
oleksii_laptop <formovka4@ukr.net>
2025-09-08 09:35:33 +0300

    created new test to check corporate colors

---
+++ app/pages/AdminPage/helpers.ts
@@ -0,0 +1,8 @@
+export const defaultColors: Record<string, string> = {
+  "--oxd-primary-one-color": "#FF7B1D",
+  "--oxd-primary-font-color": "#ffffff",
+  "--oxd-secondary-four-color": "#76BC21",
+  "--oxd-secondary-font-color": "#ffffff",
+  "--oxd-primary-gradient-start-color": "#FF920B",
+  "--oxd-primary-gradient-end-color": "#F35C17",
+};
--- app/pages/pageManager.ts
+++ app/pages/pageManager.ts
@@ -3,17 +3,20 @@ import { LoginPage } from "./loginPage";
 import { PimAddEmployee } from "./PIMPage/addEmployeePage";
 import { PimEmployeeList } from "./PIMPage/employeeListPage";
 import { AdminUserManagement } from "./AdminPage/userManagementPage";
+import { CorporateBranding } from "./AdminPage/corporateBrandingPage";
 
 export class PageManager {
   loginPage: LoginPage;
   addEmployeePage: PimAddEmployee;
   employeeListPage: PimEmployeeList;
   adminUserManagement: AdminUserManagement;
+  corporateBranding: CorporateBranding;
 
   constructor(page: Page) {
     this.loginPage = new LoginPage(page);
     this.addEmployeePage = new PimAddEmployee(page);
     this.employeeListPage = new PimEmployeeList(page);
     this.adminUserManagement = new AdminUserManagement(page);
+    this.corporateBranding = new CorporateBranding(page);
   }
 }
